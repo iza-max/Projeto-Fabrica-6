342d1cb33ad3710db979597f30ce9d18eee27c46
iza-max <izzadora.ribeiro3@gmail.com>
2025-10-30 10:20:48 -0300

    projeto6

---
+++ Create_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE aluno (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+nome_completo TEXT NOT NULL,
+data_nascimento DATE NOT NULL,
+email TEXT NOT NULL,
+telefone TEXT NOT NULL,
+endereco TEXT NOT NULL,
+curso_matriculado TEXT NOT NULL,
+status_matricula TEXT NOT NULL
+)
+
+
+
+SELECT * FROM aluno WHERE status_matricula = "Ativo"
